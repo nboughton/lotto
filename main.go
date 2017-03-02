ba9b5ae6414ed0ca3d16a2f7af450c9aaca98242
Nick Boughton <nicholasboughton@gmail.com>
2017-03-02 10:32:20 +0000

    intial imports for js/css and base level templates

--- main.go
+++ main.go
@@ -1,42 +1,16 @@
 package main
 
 import (
-	"fmt"
-	"math/rand"
-	"time"
+  "github.com/pilu/traffic"
 )
 
-var (
-	baseURL = "https://www.lottery.co.uk/lotto/results/archive-1994"
-)
+var router *traffic.Router
 
-/* @TODO: rewrite generator to create num array and remove selected numbers every time
-write scraper and db code to generate aggregate dataset with web front end to
-view data.
-*/
-
-func main() {
-	max, res := 59, [][]int{}
-	rand.Seed(time.Now().Unix())
-	for i := 0; i < 1000; i++ {
-		m := []int{}
-		for len(m) < 6 {
-			n := rand.Intn(max) + 1
-			if !contains(n, m) {
-				m = append(m, n)
-			}
-		}
-		res = append(res, m)
-		//time.Sleep(time.Second)
-	}
-	fmt.Println(res[rand.Intn(len(res))])
+func init() {
+  router = traffic.New()
+  router.Get("/", RootHandler)
 }
 
-func contains(n int, s []int) bool {
-	for _, v := range s {
-		if v == n {
-			return true
-		}
-	}
-	return false
+func main() {
+  router.Run()
 }
