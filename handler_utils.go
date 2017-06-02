15e3f92a0a1ebb969865d88fc875da16ba23663c
Nick Boughton <nicholasboughton@gmail.com>
2017-06-02 14:58:21 +0100

    modified main table

--- handler_utils.go
+++ handler_utils.go
@@ -36,8 +36,8 @@ func params(r *http.Request) queryParams {
 
 func createMainTableData(p queryParams) []TableRow {
 	var (
-		sBalls, sBonus = getSortedResultsByFreq(p)
-		most, least    = getMostAndleast(sBalls, sBonus)
+		sBalls, sBonus, byPosition                                 = getSortedResultsByFreq(p)
+		mostOverall, leastOverall, mostByPosition = getMostAndleast(sBalls, sBonus, byPosition)
 	)
 
 	last, err := db.getLastDraw()
@@ -51,12 +51,16 @@ func createMainTableData(p queryParams) []TableRow {
 			Num:   last,
 		},
 		TableRow{
-			Label: "Most Frequent",
-			Num:   most,
+			Label: "Most Frequent (overall)",
+			Num:   mostOverall,
 		},
 		TableRow{
-			Label: "Least Frequent",
-			Num:   least,
+			Label: "Most Frequent (position)",
+			Num:   mostByPosition,
+		},
+		TableRow{
+			Label: "Least Frequent (overall)",
+			Num:   leastOverall,
 		},
 		TableRow{
 			Label: "Random Set",
@@ -65,45 +69,62 @@ func createMainTableData(p queryParams) []TableRow {
 	}
 }
 
-func getMostAndleast(sBalls, sBonus sortByFreq) (most, least []int) {
-	// most = first 6 and least = last non-zero 6
+func getMostAndleast(sBalls, sBonus sortByFreq, byPosition []sortByFreq) (mostOverall, leastOverall, mostByPosition []int) {
+	// mostOverall = first 6 and leastOverall = last non-zero 6
 	for i, j := 0, len(sBalls)-1; i < j; i, j = i+1, j-1 {
-		if sBalls[i].num != 0 && len(most) < 6 {
-			most = append(most, sBalls[i].num)
+		if sBalls[i].num != 0 && len(mostOverall) < 6 {
+			mostOverall = append(mostOverall, sBalls[i].num)
 		}
-		if sBalls[j].num != 0 && len(least) < 6 {
-			least = append(least, sBalls[j].num)
+		if sBalls[j].num != 0 && len(leastOverall) < 6 {
+			leastOverall = append(leastOverall, sBalls[j].num)
 		}
-		if len(most) == 6 && len(least) == 6 {
+		if len(mostOverall) == 6 && len(leastOverall) == 6 {
 			break
 		}
 	}
 
 	// Sort the results, this is largely cosmetic.
-	sort.Ints(most)
-	sort.Ints(least)
+	sort.Ints(mostOverall)
+	sort.Ints(leastOverall)
 
-	// Add the bonus ball for most/least frequent, don't duplicate numbers
+	// Add the bonus ball for mostOverall/leastOverall frequent, don't duplicate numbers
 	for i, j := len(sBonus)-1, 0; i > j; i, j = i-1, j+1 {
-		if sBonus[i].num != 0 && len(most) < balls && !containsInt(most, sBonus[i].num) {
-			most = append(most, sBonus[i].num)
+		if sBonus[i].num != 0 && len(mostOverall) < balls && !containsInt(mostOverall, sBonus[i].num) {
+			mostOverall = append(mostOverall, sBonus[i].num)
 		}
-		if sBonus[j].num != 0 && len(least) < balls && !containsInt(least, sBonus[j].num) {
-			least = append(least, sBonus[j].num)
+		if sBonus[j].num != 0 && len(leastOverall) < balls && !containsInt(leastOverall, sBonus[j].num) {
+			leastOverall = append(leastOverall, sBonus[j].num)
 		}
-		if len(most) == balls && len(least) == balls {
+		if len(mostOverall) == balls && len(leastOverall) == balls {
 			break
 		}
 	}
-	return most, least
+
+	// get most and least byPosition set
+	mostByPosition = make([]int, balls)
+
+	for i := range byPosition {
+		mostByPosition[i] = byPosition[i][0].num
+	}
+
+	mostByPosition[6] = mostOverall[6]
+
+	return mostOverall, leastOverall, mostByPosition
 }
 
 // Returns sorted ball results for query p by frequency as well as collated numbers for mode checking
-func getSortedResultsByFreq(p queryParams) (sortByFreq, sortByFreq) {
+func getSortedResultsByFreq(p queryParams) (sortByFreq, sortByFreq, []sortByFreq) {
 	var (
-		sBalls = make(sortByFreq, maxBallNum+1)
-		sBonus = make(sortByFreq, maxBallNum+1)
+		sBalls     = make(sortByFreq, maxBallNum+1)
+		sBonus     = make(sortByFreq, maxBallNum+1)
+		byPosition = make([]sortByFreq, balls)
 	)
+
+	// Set up byPosition arrays of numbers
+	for i := range byPosition {
+		byPosition[i] = make(sortByFreq, maxBallNum+1)
+	}
+
 	for row := range db.getResults(p) {
 		for ball := 0; ball < balls; ball++ {
 			n := row.Num[ball]
@@ -111,6 +132,10 @@ func getSortedResultsByFreq(p queryParams) (sortByFreq, sortByFreq) {
 				// Collate total frequncies for first 6
 				sBalls[n].num = n
 				sBalls[n].freq++
+
+				// Collate sorted results by position
+				byPosition[ball][n].num = n
+				byPosition[ball][n].freq++
 			} else {
 				// Collate frequencies for bonus ball separately
 				sBonus[n].num = n
@@ -119,10 +144,15 @@ func getSortedResultsByFreq(p queryParams) (sortByFreq, sortByFreq) {
 		}
 	}
 
+	// Sort byPosition arrays
+	for i := range byPosition {
+		sort.Sort(sort.Reverse(byPosition[i]))
+	}
+
 	// Sort both lists
 	sort.Sort(sort.Reverse(sBalls))
 	sort.Sort(sBonus)
-	return sBalls, sBonus
+	return sBalls, sBonus, byPosition
 }
 
 func containsInt(a []int, t int) bool {
