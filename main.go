4a3022a95f9182136edc1415fded867f4167d571
Nick Boughton <nicholasboughton@gmail.com>
2017-07-19 13:08:35 +0100

    added request logging

--- main.go
+++ main.go
@@ -5,13 +5,16 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"time"
 
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
 
 type config struct {
 	Port int
+	Log  bool
 }
 
 var (
@@ -21,9 +24,11 @@ var (
 
 func init() {
 	p := flag.Int("p", 3002, "Set the port the application listens on")
+	l := flag.Bool("l", true, "Log requests to STDOUT")
 	flag.Parse()
 
 	cfg.Port = *p
+	cfg.Log = *l
 
 	// Update at 21:30 every night
 	go func() {
@@ -43,5 +48,20 @@ func main() {
 	r.HandleFunc("/query", handlerQuery).Methods("GET")
 	r.PathPrefix("/").Handler(http.FileServer(http.Dir("public/")))
 
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", cfg.Port), r))
+	if cfg.Log {
+		log.Fatal(
+			http.ListenAndServe(
+				fmt.Sprintf(":%d", cfg.Port),
+				handlers.LoggingHandler(os.Stdout, r),
+			),
+		)
+	} else {
+		log.Fatal(
+			http.ListenAndServe(
+				fmt.Sprintf(":%d", cfg.Port),
+				r,
+			),
+		)
+	}
+
 }
